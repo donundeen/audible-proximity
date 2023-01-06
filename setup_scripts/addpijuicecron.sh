8f4f297611e65fbd24dc53307f4cedcae6db38c8
don undeen <donundeen@yahoo.com>
2023-01-06 09:58:01 -0500

    added stderr

--- setup_scripts/addpijuicecron.sh
+++ setup_scripts/addpijuicecron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod a+x /home/pi/audible-proximity/pijuice_utils/pijuice_utils.py
-line="5 * * * * /usr/bin/date >> /home/pi/juicemonitor.txt ; /home/pi/audible-proximity/pijuice_utils/pijuice_utils.py --get-input >> /home/pi/juicemonitor.txt; /home/pi/audible-proximity/pijuice_utils/pijuice_utils.py --get-battery >> /home/pi/juicemonitor.txt"
+line="5 * * * * /usr/bin/date >> /home/pi/juicemonitor.txt  2>&1; /home/pi/audible-proximity/pijuice_utils/pijuice_utils.py --get-input >> /home/pi/juicemonitor.txt 2>&1; /home/pi/audible-proximity/pijuice_utils/pijuice_utils.py --get-battery >> /home/pi/juicemonitor.txt  2>&1"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
