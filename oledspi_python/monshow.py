62505fc371d795b40c2977934be8cb706f2df309
don undeen <donundeen@yahoo.com>
2023-02-05 13:55:00 -0500

    Update monshow.py

--- oledspi_python/monshow.py
+++ oledspi_python/monshow.py
@@ -89,7 +89,7 @@ while True:
 
     try:
         # get the power info from juicemonitor file
-        line = subprocess.check_output(['tail', '-1', JUICEFILE]) 
+        line = subprocess.check_output(['tail', '-1', JUICEFILE]).decode("UTF-8") 
         line = line.replace("'","\"")
         print ("line::")
         print (line)
