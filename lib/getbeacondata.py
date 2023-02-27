be87f0f473907886708f9fb8adfa772ca99e4675
don undeen <donundeen@yahoo.com>
2023-02-26 19:59:16 -0500

    Create getbeacondata.py

---
+++ lib/getbeacondata.py
@@ -0,0 +1,23 @@
+import pigpio
+import time
+import sys
+
+pi = None
+slave_addr = 0x12
+
+def i2cInterrupt(event, tick):
+   global pi
+   global slave_addr
+   status, bytes_read, data = pi.bsc_i2c(slave_addr)
+
+   if bytes_read:
+      print(data.decode("utf-8"))
+      sys.stdout.flush()
+
+pi = pigpio.pi()
+int_handler = pi.event_callback(pigpio.EVENT_BSC, i2cInterrupt)
+pi.bsc_i2c(slave_addr)
+
+while True:
+   print("stil here")
+   time.sleep(10)
